0a886c269011dae92008cba7af994adef9ed7520
Borges Camilla <camillaborgeshelena@gmail.com>
2022-01-14 10:02:41 +0100

    implementing input logic for number of teams

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,11 +9,18 @@ export class AppComponent {
   newMemberName = "";
   members: string[] = [];
   errorMessage = "";
+  numberOfTeams = 0;
 
   onInput(member: string) {
     this.newMemberName = member;
     console.log(this.newMemberName)
   }
+
+  onNumberOfTeamsInput(value: string) {
+    this.numberOfTeams = Number(value);
+    
+  }
+
   addMember() {
     if (!this.newMemberName) {
       this.errorMessage = "Attention! Name can't be empty"
